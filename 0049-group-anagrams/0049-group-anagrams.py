e6b8f07e6b3176a65ac1f443ca96dfaf8d2fcf41
Yash Kumar <117767437+Yashcabin6@users.noreply.github.com>
2025-12-15 17:38:09 +0530

    Time: 19 ms (36.99%), Space: 22.7 MB (24.34%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,4 +1,4 @@
-
+from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         # ok so objective is to make array of groups of anagrams how will I do this?
@@ -9,16 +9,13 @@ class Solution:
         # one important syntax is included ok what is it??? query_map = defaultdict(list), this will make map to list by default
         if len(strs)==1:
             return [strs]
-        query_map = {}
+        query_map = defaultdict(list)
         for s in strs:
             query = [0]*26
             for c in s:
                 query[ord(c)-ord('a')]+=1
             key = tuple(query)
-            if key in query_map:
-                query_map[key].append(s)
-            else:
-                query_map[key]= [s]
+            query_map[key].append(s)
         ret = []
         for key in query_map:
             ret.append(query_map[key])
