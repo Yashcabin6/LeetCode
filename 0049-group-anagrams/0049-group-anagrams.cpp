e7cff18dd3725f697c122e22967ba66a225a84b4
Yash Kumar <117767437+Yashcabin6@users.noreply.github.com>
2025-12-15 17:53:32 +0530

    Time: 76 ms (7.15%), Space: 37.6 MB (5.3%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // to_string() operation is unlike toupper() and tolower()
+        // in maps if u get key pointer or refrence it's different please notice that so basically unordered map is a pair data structure not a map so that's why if u have a key you wont use it as arguemnt key : query_map gives you item key reference
+        if(strs.size()==1) return {strs};
+        unordered_map<string,vector<string>> query_map;
+        for(string s : strs){
+            vector<int> query(26,0);
+            for(char c: s){
+                query[c-'a']+=1;
+            }
+            string key = "";
+            for(int i : query){
+                key+=to_string(i)+'.';
+            }
+            query_map[key].push_back(s);
+        }
+        vector<vector<string>> ret;
+        for(auto key : query_map){
+            ret.push_back(key.second);
+        }
+        return ret;
+    }
+};
