b5bd9285d5f2ee06158ca651aa25973a636f5510
Yash Kumar <117767437+Yashcabin6@users.noreply.github.com>
2025-12-16 07:33:01 +0530

    Time: 47 ms (9.92%), Space: 26.1 MB (30.22%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,14 +4,14 @@ class Solution:
         n = len(nums)
         right_query = [1]*n
         left_query = [1]*n
-        left_query[0]*=nums[0]
-        right_query[n-1]*=nums[n-1]
+        left_query[0]=nums[0]
+        right_query[n-1]=nums[n-1]
         for i in range(1,n):
             left_query[i]*=left_query[i-1]*nums[i]
             right_query[-i-1]*=right_query[-i]*nums[-i-1]
         ret = [1]*n
-        ret[0]*= right_query[1]
-        ret[-1]*= left_query[n-2]
+        ret[0] = right_query[1]
+        ret[-1] = left_query[n-2]
         for i in range(1,n-1):
             ret[i]=left_query[i-1]*right_query[i+1]
         return ret
